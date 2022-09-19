bca5e3ca16955bd8780a84a0b435862463efb6fb
Fav-chi <105231924+Fav-chi@users.noreply.github.com>
2022-09-19 20:41:47 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN
-#define MAIN_
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -8,12 +8,8 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-
-void puts2(char *str
+void puts2(char *str);
 void puts_half(char *str);
-
 void print_array(int *a, int n);
-
 char *_strcpy(char *dest, char *src);
-
-#endi
+#endif
